b3ebecce32cc1c3be9941697489df25d24081887
NayeemHossenJim <155984091+NayeemHossenJim@users.noreply.github.com>
2024-10-12 21:26:46 +0600

    Player Movement

---
+++ 2D/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5.5f;
+
+    private Rigidbody2D rb;
+    private float moveInput;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        moveInput = UserInput.moveInput.x;
+
+        rb.velocity = new Vector2 (moveInput*moveSpeed,rb.velocity.y);
+    }
+}
